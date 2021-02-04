20164f03c53ba45dcf4e1f80694f10eed6271c92
James Grasso <grassojames5@gmail.com>
2021-02-04 22:09:06 +1100

    ugh

--- tests/test.js
+++ tests/test.js
@@ -2,13 +2,6 @@ const logger = require('../index');
 
 logger.log("Hey y'all wassup!", __dirname, __filename);
 
-logger.log(
-    'Oopsie I made a booboo and this is an error',
-    __dirname,
-    __filename,
-    'error',
-);
+logger.log('Oopsie I made a booboo and this is an error', __dirname, __filename, 'error');
 
 logger.log("Finished Successfully!", __dirname, __filename, 'good');
-
-process.exit(0);
