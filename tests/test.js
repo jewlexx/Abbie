2a65e964e93d187f1f578c4d912e2a3826943dc9
James Grasso <grassojames5@gmail.com>
2021-02-04 21:54:00 +1100

    Fixed caps

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ const logger = require('../index');
 logger.log("Hey y'all wassup!", __dirname, __filename);
 
 logger.log(
-    'Oopsie i made a booboo and this is an error',
+    'Oopsie I made a booboo and this is an error',
     __dirname,
     __filename,
     'error',
