d6a6cafbaf00c71c3e767cf688798025e38fb718
jamesafk <grassojames5@gmail.com>
2021-02-10 12:36:15 +1100

    Actually fixed it I think

--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-import logger from '../index';
-
-logger.log("Hey y'all wassup!");
-
-logger.log('Oopsie I made a booboo and this is an error', 'error');
-
-logger.log("Finished Successfully!", 'good');
