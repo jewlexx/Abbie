cd8d2245ec2fe7601d47fc45fcc2f50d09f733fe
James Grasso <grassojames5@gmail.com>
2021-02-11 16:45:09 +1100

    Updated some stuff

---
+++ tests/TSTest.ts
@@ -0,0 +1,8 @@
+require('dotenv').config();
+import abbie from '../index';
+
+abbie.log("Hey y'all wassup!");
+
+abbie.log('Oopsie I made a booboo and this is an error', 1);
+
+abbie.log('Finished Successfully!', 2);
--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-import log from '../index';
-
-log.log("Hey y'all wassup!");
-
-log.log('Oopsie I made a booboo and this is an error', 1);
-
-log.log("Finished Successfully!", 2);
